6fd9fad2cb166b41ad6257a77623f623e62ced15
sea-lantern <kaiki.yamada@geniee.co.jp>
2024-10-02 01:30:35 +0900

    add: decide sell price by rarity

--- src/main/java/com/yoshinani/shopButton/SellButton.java
+++ src/main/java/com/yoshinani/shopButton/SellButton.java
@@ -1,5 +1,7 @@
 package com.yoshinani.shopButton;
 
+import com.robertx22.mine_and_slash.saveclasses.item_classes.GearItemData;
+import com.robertx22.mine_and_slash.uncommon.datasaving.StackSaving;
 import com.yoshinani.customTrader.CustomChestMenu;
 import com.yoshinani.money.Money;
 import net.minecraft.network.chat.Component;
@@ -7,9 +9,23 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class SellButton implements ShopButton {
+    private static final Map<String, Integer> RARITY_PRICE_MAP = new HashMap<String, Integer>() {
+        {
+            put("common", 100);
+            put("uncommon", 200);
+            put("rare", 300);
+            put("epic", 400);
+            put("legendary", 500);
+            put("mythic", 600);
+            put("unique", 700);
+            put("runeword", 800);
+        }
+    };
     public String itemId;
     public int slotId;
     public String displayName;
@@ -47,8 +63,9 @@ public class SellButton implements ShopButton {
         int sellingPrice = 0;
         for (int i = 0; i < 45; i++) {
             ItemStack stack = parent.container.getItem(i);
-            if (!stack.isEmpty()) {
-                sellingPrice += stack.getCount() * 100;
+            GearItemData data = StackSaving.GEARS.loadFrom(stack);
+            if (!stack.isEmpty() && data != null) {
+                sellingPrice += stack.getCount() * RARITY_PRICE_MAP.get(data.rar);;
                 parent.container.setItem(i, ItemStack.EMPTY);
             }
         }
