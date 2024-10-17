c7955f3ceef98db2313ca975111d6f6a432ee7c2
sea-lantern <kaiki.yamada@geniee.co.jp>
2024-10-18 05:50:05 +0900

    feat: ux

--- src/main/java/com/yoshinani/customTrader/TheraistTrader.java
+++ src/main/java/com/yoshinani/customTrader/TheraistTrader.java
@@ -4,6 +4,8 @@ import com.yoshinani.env.CustomEnv;
 import com.yoshinani.money.Money;
 import com.yoshinani.therapist.Therapist;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.*;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
@@ -15,7 +17,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.network.chat.Component;
 import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.NotNull;
-import net.minecraft.world.Containers;
 
 public class TheraistTrader extends CustomTrader {
     public TheraistTrader(EntityType<? extends Mob> entityType, Level level) {
@@ -42,6 +43,7 @@ public class TheraistTrader extends CustomTrader {
 
                         if (totalFee >= Money.getPlayerMoney(serverPlayer)) {
                             serverPlayer.sendSystemMessage(Component.literal("お金が足りません"));
+                            cnt = 0;
                         } else {
                             Money.setPlayerMoney(serverPlayer, Money.getPlayerMoney(serverPlayer) - totalFee);
                             for (int i = 0; i < this.getContainerSize(); i++) {
@@ -54,8 +56,12 @@ public class TheraistTrader extends CustomTrader {
                         for (int i = 0; i < this.getContainerSize(); i++) {
                             ItemStack stack = this.getItem(i);
                             if (!stack.isEmpty()) {
-                                Containers.dropItemStack(serverPlayer.level(), serverPlayer.getX(), serverPlayer.getY(), serverPlayer.getZ(), stack);
+                                serverPlayer.addItem(stack);
                             }
+                        }
+                        if (cnt > 0) {
+                            serverPlayer.sendSystemMessage(Component.literal("保険をかけたアイテム数: " + cnt + " 残金: " + Money.getPlayerMoney(serverPlayer)));
+                            serverPlayer.playNotifySound(SoundEvents.PLAYER_LEVELUP, SoundSource.PLAYERS, 1.0F, 1.0F);
                         }
                     }
                 }
